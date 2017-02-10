9472d3dfde5f553f13ac9e136e138ab773428ef0
checkmate-bitch <sawan.dontgiveadamn@gmail.com>
2017-02-10 13:14:34 +0530

    background.js: allowed interaction with context menu

---
+++ background.js
@@ -0,0 +1,15 @@
+function getQWiki(info,tab) {
+  
+  // console.log("Word " + info.selectionText + " was clicked.");
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id , info.selectionText, function(response) { console.log("in resp");});  
+  });
+
+}
+
+chrome.contextMenus.create({
+  title: "Search: %s", 
+  contexts:["selection"], 
+  onclick: getQWiki
+});
