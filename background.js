fb8775bc4d761800a0c19027509450c4bce3c844
checkmate-bitch <sawan.dontgiveadamn@gmail.com>
2017-02-10 13:21:23 +0530

    background.js: alignment corrected

--- background.js
+++ background.js
@@ -3,8 +3,11 @@ function getQWiki(info,tab) {
   // console.log("Word " + info.selectionText + " was clicked.");
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    chrome.tabs.sendMessage(tabs[0].id , info.selectionText, function(response) { console.log("in resp");});  
-  });
+      chrome.tabs.sendMessage(tabs[0].id , info.selectionText, function(response) { 
+    	// console.log("in resp");
+      });  
+ 	  }
+  );
 
 }
 
