303e27fed8acd5b270766612e19bad89998c8b61
checkmate-bitch <sawan.dontgiveadamn@gmail.com>
2017-07-21 16:45:37 +0530

    responds to changes made in options without need to refresh

--- inject.js
+++ inject.js
@@ -50,7 +50,7 @@ innerDiv.classList.add("qwiki-extension-inner-div");
 handle.classList.add("qwiki-extension-handle");
 
 // format close button
-closeSpan.innerHTML = "&#10006;";
+closeSpan.innerHTML = "<span>&#10006;</span>";
 closeSpan.style.background = qwikiColorPalette.closeBackground;
 closeSpan.style.color = qwikiColorPalette.closeColor;
 
@@ -65,42 +65,42 @@ document.body.appendChild(div);
 // change the width and height of side panel or container according to the mouse position 
 // when click and drag on red resize handle
 function startResize(e){
-  e.preventDefault();
-  // console.log(e.clientY, div.offsetTop);
-  div.style.width = (window.innerWidth - e.clientX - 30) + "px";
-  div.style.height = (e.clientY - 100) + "px";
+    e.preventDefault();
+    // console.log(e.clientY, div.offsetTop);
+    div.style.width = (window.innerWidth - e.clientX - 30) + "px";
+    div.style.height = (e.clientY - 100) + "px";
 }
 
 // remove events when mouse no longer dragging resize handle
 function stopResize(){
-  window.removeEventListener("mousemove", startResize);
-  window.removeEventListener("mouseup", stopResize);
+    window.removeEventListener("mousemove", startResize);
+    window.removeEventListener("mouseup", stopResize);
 }
 
 // checks for mousemovement and mouseup
 function initResize(){
-  window.addEventListener("mousemove", startResize);
-  window.addEventListener("mouseup", stopResize);
+    window.addEventListener("mousemove", startResize);
+    window.addEventListener("mouseup", stopResize);
 }
 
 // remove the side panel when close button clicked
 function closePanel(){
-  div.style.height = "0";
-  div.style.width = "0";
-  div.style.opacity = "0";
-  div.style.padding = "0";
-  innerDiv.style.padding = "0";
+    div.style.height = "0";
+    div.style.width = "0";
+    div.style.opacity = "0";
+    div.style.padding = "0";
+    innerDiv.style.padding = "0";
 }
 
 // check if class "hide" is added. If yes, then remove else add
 function unhide(e){
-  // console.log(e);
-  if(e.target.localName == "h2"){
-    var hide = e.srcElement.parentElement.children[1];
-    if(!hide.classList.length)
-      hide.classList.add("qwiki-extension-hide")
-    else hide.classList.remove("qwiki-extension-hide");
-  }
+    // console.log(e);
+    if(e.target.localName == "h2"){
+        var hide = e.srcElement.parentElement.children[1];
+        if(!hide.classList.length)
+            hide.classList.add("qwiki-extension-hide")
+        else hide.classList.remove("qwiki-extension-hide");
+    }
 }
 
 
@@ -113,109 +113,109 @@ closeSpan.addEventListener("click", closePanel);
 // side panel styling
 // needed whenever container closed and reopened
 function styleSidePanel(){
-  
-  // transition side panel
-  div.style.width = "400px";
-  div.style.height = "75vh";
-  div.style.opacity = "1";
-  div.style.borderRadius = "3px";
-  div.style.padding = "50px 10px 0 10px";
-  div.style.transition = `opacity 0.5s ease-in-out`;
-  div.style.top = window.scrollY + 55 +"px";
-  // div.style.transition = `translateY 1s cubic-bezier(0.68, -0.85, 0.265, 1.55)`;
 
-  // inner div padding
-  innerDiv.style.padding = "0 10px 0 10px";
+    // transition side panel
+    div.style.width = "400px";
+    div.style.height = "75vh";
+    div.style.opacity = "1";
+    div.style.borderRadius = "3px";
+    div.style.padding = "50px 10px 0 10px";
+    div.style.transition = `opacity 0.5s ease-in-out`;
+    div.style.top = window.scrollY + 55 +"px";
+    // div.style.transition = `translateY 1s cubic-bezier(0.68, -0.85, 0.265, 1.55)`;
+
+    // inner div padding
+    innerDiv.style.padding = "0 10px 0 10px";
 
 }
 
 function getString(){
 
-  console.log(window.getSelection().toString());
-  
-  // get the word on which the event double click triggered
-  var original_val = window.getSelection().toString();
+    console.log(window.getSelection().toString());
 
-  // format val for spaces
-  var val = original_val.trim().replace(/\s/g, "_");
+    // get the word on which the event double click triggered
+    var original_val = window.getSelection().toString();
 
-  // url to get data from
-  var url = "https://en.wikipedia.org/w/api.php?action=parse&prop=text&page="+val+"&format=json&redirects=1";
+    // format val for spaces
+    var val = original_val.trim().replace(/\s/g, "_");
 
-  // get the data, format it and put it in the side panel 
-  $.getJSON(url, data => {
+    // url to get data from
+    var url = "https://en.wikipedia.org/w/api.php?action=parse&prop=text&page="+val+"&format=json&redirects=1";
 
-    styleSidePanel();
+    // get the data, format it and put it in the side panel 
+    $.getJSON(url, data => {
 
-    // If no result returned
-    if(data.error){
-      // alert("No result for "+val);
-      console.log("lets see\n");
-      val = val.replace(/_/g, " ");
-      innerDiv.innerHTML = `<p style="color: #f1f1f1;">No result available for <h2 style="overflow-wrap : break-word; color : coral">${val}</h2> </p>`;
-      return;
-    }
+        styleSidePanel();
 
-    // console.log(data.parse.text["*"]);
-    
-    // get the markup in a div container which gives htmlcollection[] object
-    innerDiv.innerHTML = data.parse.text["*"];
+        // If no result returned
+        if(data.error){
+            // alert("No result for "+val);
+            console.log("lets see\n");
+            val = val.replace(/_/g, " ");
+            innerDiv.innerHTML = `<p style="color: #f1f1f1;">No result available for <h2 style="overflow-wrap : break-word; color : coral">${val}</h2> </p>`;
+            return;
+        }
 
-    // console.log(innerDiv.children[0].children);
+        // console.log(data.parse.text["*"]);
 
-    // convert the htmlcollection datatype to array and remove unwanted properties
-    var children = Array.from(innerDiv.children[0].children);
+        // get the markup in a div container which gives htmlcollection[] object
+        innerDiv.innerHTML = data.parse.text["*"];
+
+        // console.log(innerDiv.children[0].children);
+
+        // convert the htmlcollection datatype to array and remove unwanted properties
+        var children = Array.from(innerDiv.children[0].children);
         children = children.filter( child => {
-          // console.log(child.tagName);
-          return child.tagName !== "TABLE" && child.tagName !== "DIV"; 
+            // console.log(child.tagName);
+            return child.tagName !== "TABLE" && child.tagName !== "DIV"; 
         });
 
-    // format the array, convert it into string
-    // parser that removes wiki markup and adds html
-    var html = children.map( child => {
-                if(child.tagName === "P")
-                  return `<p class="qwiki-extension-para-graph" style="color: ${qwikiColorPalette.paraColor} !important;">${child.innerText}</p>`; 
-                else if(child.tagName === "H2")
-                  return `</article></div><div id="${child.innerText.replace(/\[\w+\]/, "")}"><h2 class="qwiki-extension-heading2" style="color: ${qwikiColorPalette.subHeading} !important;">${child.innerText}</h2><article class="qwiki-extension-hide">`;
-                else if(child.tagName === "H3")
-                  return `<h3 class="qwiki-extension-heading3" style="color: ${qwikiColorPalette.subHeadingH3} !important;">${child.innerText}</h3>`;
-                else if(child.tagName === "UL")
-                  return `<ul class="qwiki-extension-para-graph" style="color: ${qwikiColorPalette.fontColor} !important;">${child.innerText}</ul>`;
-                else return;
-              }) 
-              .join("")
-              .replace(/\[\w+\]/g, "");
+        // format the array, convert it into string
+        // parser that removes wiki markup and adds html
+        var html = children.map( child => {
+            if(child.tagName === "P")
+                return `<p class="qwiki-extension-para-graph" style="color: ${qwikiColorPalette.paraColor} !important;">${child.innerText}</p>`; 
+            else if(child.tagName === "H2")
+                return `</article></div><div id="${child.innerText.replace(/\[\w+\]/, "")}"><h2 class="qwiki-extension-heading2" style="color: ${qwikiColorPalette.subHeading} !important;">${child.innerText}</h2><article class="qwiki-extension-hide">`;
+            else if(child.tagName === "H3")
+                return `<h3 class="qwiki-extension-heading3" style="color: ${qwikiColorPalette.subHeadingH3} !important;">${child.innerText}</h3>`;
+            else if(child.tagName === "UL")
+                return `<ul class="qwiki-extension-para-graph" style="color: ${qwikiColorPalette.paraColor} !important;">${child.innerText}</ul>`;
+            else return;
+        }) 
+            .join("")
+            .replace(/\[\w+\]/g, "");
 
-    // remove extra div and article tags from the begining
-    html = html.replace("</article></div>", "");
-    html = `<h1 class="qwiki-extension-panel-h1"><a title="link to Wiki" href="https://en.wikipedia.org/wiki/${val}" target="_blank">${original_val}</a></h1>` + html;    
-    
-    // Get the indexes of See also , Footnotes, References, External Links and other redundant headings
-    var see = html.indexOf("See also") === -1 ? html.length : html.indexOf("See also");
-    var ref = html.indexOf("References") === -1 ? html.length : html.indexOf("References");
-    var ext = html.indexOf("External links") === -1 ? html.length : html.indexOf("External links");
-    var foot = html.indexOf("Footnotes") === -1 ? html.length : html.indexOf("Footnotes");
-    // store the minimum
-    var min = Math.min(see, ref, ext, foot);
-    // if none of these are present in the document then -1 will be displayed
-    var index = min === -1 ? html.length : min; 
-    // console.log("all",{see,ref,ext,foot});
-    // console.log(index);
-    
-    // get string only till useful content
-    // append formatted string into innerDiv
+        // remove extra div and article tags from the begining
+        html = html.replace("</article></div>", "");
+        html = `<h1 class="qwiki-extension-panel-h1"><a style="color: ${qwikiColorPalette.mainHeading} !important" title="link to Wiki" href="https://en.wikipedia.org/wiki/${val}" target="_blank">${original_val}</a></h1>` + html;    
 
-    innerDiv.innerHTML = html.substring(0, index);
+        // Get the indexes of See also , Footnotes, References, External Links and other redundant headings
+        var see = html.indexOf("See also") === -1 ? html.length : html.indexOf("See also");
+        var ref = html.indexOf("References") === -1 ? html.length : html.indexOf("References");
+        var ext = html.indexOf("External links") === -1 ? html.length : html.indexOf("External links");
+        var foot = html.indexOf("Footnotes") === -1 ? html.length : html.indexOf("Footnotes");
+        // store the minimum
+        var min = Math.min(see, ref, ext, foot);
+        // if none of these are present in the document then -1 will be displayed
+        var index = min === -1 ? html.length : min; 
+        // console.log("all",{see,ref,ext,foot});
+        // console.log(index);
 
-    // add all inner divs to un/hide
-    hiders = Array.from(innerDiv.querySelectorAll("div"));
-    hiders.forEach( hider => {
-      // console.log(hider);
-      hider.addEventListener("click", unhide);
-    });
-    // beginHiders;
+        // get string only till useful content
+        // append formatted string into innerDiv
 
-  });   
+        innerDiv.innerHTML = html.substring(0, index);
+
+        // add all inner divs to un/hide
+        hiders = Array.from(innerDiv.querySelectorAll("div"));
+        hiders.forEach( hider => {
+            // console.log(hider);
+            hider.addEventListener("click", unhide);
+        });
+        // beginHiders;
+
+    });   
 }
 
 if(!qwikiIsDbClick) {
@@ -248,11 +248,13 @@ function restore_options() {
         if(isDbClick) {
             document.addEventListener("dblclick", getString);
         }
+        else {
+            document.removeEventListener("dblclick", getString);
+        }
         // console.log("db click", qwikiIsDbClick);
         // console.log("qcp after", qwikiColorPalette);
     });
 }
 
-// document.addEventListener('DOMContentLoaded', restore_options);
+chrome.storage.onChanged.addListener(restore_options);
 $(document).ready(restore_options);
-// restore_options();
